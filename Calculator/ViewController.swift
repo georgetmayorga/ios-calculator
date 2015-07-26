45cc0444387bf19f84d60dfaa435243ad76020ed
George Mayorga <georgetmayorga@gmail.com>
2015-07-25 22:37:54 -0400

    initial commit

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,17 +9,69 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsInTheMiddleOfTypingANumber = false
+    
+//    input from numbers
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTypingANumber {
+            display.text = display.text! + digit
+        } else {
+            display.text = digit
+            userIsInTheMiddleOfTypingANumber = true
+        }
+    }
+    
+//    define array that will hold memory of inputs...stack
+    var operandStack = Array<Double>()
+    
+//    what happens when you hit enter
+    @IBAction func enter() {
+        userIsInTheMiddleOfTypingANumber = false
+        operandStack.append(displayValue)
+        println("stack = \(operandStack)")
+    }
+    
+//    this converts the string into a double and sets a double as a string
+    var displayValue: Double {
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set {
+            display.text = "\(newValue)"
+        }
+    }
+    
+//    passing a function (operation) to a function (performOperation), that passed in function named operation takes 2 doubles and returns a double
+    func binaryOperation(operation: (Double, Double) -> Double) {
+        if operandStack.count >= 2 {
+            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+            enter()
+        }
+    }
+    
+    func unaryOperation(operation: Double -> Double) {
+        if operandStack.count >= 1 {
+            displayValue = operation(operandStack.removeLast())
+            enter()
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+//    we call perform operation and send it anonymous variables somehow, type is inferred
+    @IBAction func operate(sender: UIButton) {
+        if userIsInTheMiddleOfTypingANumber{
+            enter()
+        }
+        let operation = sender.currentTitle!
+        switch operation {
+        case "×": binaryOperation {$0 * $1}
+        case "÷": binaryOperation {$1 / $0}
+        case "+": binaryOperation {$1 - $0}
+        case "√": unaryOperation { sqrt($0) }
+        default: break
+        }
     }
-
-
 }
 
